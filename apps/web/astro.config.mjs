fac82cc8f2c6ea3b8f8a297201656a5ea3bf1122
leonardcser <73912641+leonardcser@users.noreply.github.com>
2025-08-19 21:13:00 +0200

    feat: prototype landing page

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -5,6 +5,8 @@ import cloudflare from "@astrojs/cloudflare";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   output: "static",
@@ -16,4 +18,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [icon()],
 });
