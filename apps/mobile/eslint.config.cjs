ec9331cd81357f74525950722a89d9c915d6442d
leonardcser <73912641+leonardcser@users.noreply.github.com>
2025-08-19 15:23:00 +0200

    fix: tests

--- apps/mobile/eslint.config.cjs
+++ apps/mobile/eslint.config.cjs
@@ -1,9 +1,21 @@
 const defineConfig = require("eslint/config").defineConfig;
 const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
 const expoConfig = require("eslint-config-expo/flat");
+const globals = require("globals");
 
 module.exports = defineConfig([
   { ignores: ["dist", ".expo/*", ".wrangler/*", "node_modules/*"] },
   expoConfig,
+  {
+    files: [
+      "**/__tests__/**/*.{js,jsx,ts,tsx}",
+      "**/*.{test,spec}.{js,jsx,ts,tsx}",
+    ],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
+    },
+  },
   eslintPluginPrettierRecommended,
 ]);
