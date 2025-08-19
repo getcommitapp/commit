0658d8dd9bf2fed23e62509cc39eb385123d9bd4
leonardcser <73912641+leonardcser@users.noreply.github.com>
2025-08-19 21:31:45 +0200

    style: format code

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -3,7 +3,7 @@ import eslintPluginAstro from "eslint-plugin-astro";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default defineConfig([
-  { ignores: [".astro/*", ".wrangler/*", "node_modules/*"] },
+  { ignores: ["dist/*", ".astro/*", ".wrangler/*", "node_modules/*"] },
   // add more generic rule sets here, such as:
   // js.configs.recommended,
   ...eslintPluginAstro.configs["flat/recommended"], // In CommonJS, the `flat/` prefix is required.
