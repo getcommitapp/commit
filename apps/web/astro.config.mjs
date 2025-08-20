7a0591d2d01392ba9e4a619688797199c4c1aa98
leonardcser <73912641+leonardcser@users.noreply.github.com>
2025-08-20 10:27:50 +0200

    feat(web): simplify wrangler config

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,8 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import cloudflare from "@astrojs/cloudflare";
-
 import tailwindcss from "@tailwindcss/vite";
 
 import icon from "astro-icon";
@@ -10,10 +8,6 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-
-  adapter: cloudflare({
-    imageService: "cloudflare",
-  }),
 
   vite: {
     plugins: [tailwindcss()],
