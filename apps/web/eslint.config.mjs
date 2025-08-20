a86d6f092d414aca9f723450a2f604fc9990211b
leonardcser <73912641+leonardcser@users.noreply.github.com>
2025-08-20 11:36:35 +0200

    fix(web): formatting and lint errors

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from "eslint/config";
 import eslintPluginAstro from "eslint-plugin-astro";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default defineConfig([
   { ignores: ["dist/*", ".astro/*", ".wrangler/*", "node_modules/*"] },
   // add more generic rule sets here, such as:
   // js.configs.recommended,
   ...eslintPluginAstro.configs["flat/recommended"], // In CommonJS, the `flat/` prefix is required.
-  eslintPluginPrettierRecommended,
   {
     rules: {
       // override/add rules settings here, such as:
