a1c152e3f771c403947848c9a8cc062df52d6cfe
Leonard <73912641+leonardcser@users.noreply.github.com>
2025-08-21 09:21:42 +0200

    Merge pull request #16 from getcommitapp/11-creating-landing-page
    Creating landing page

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,15 +1,22 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import cloudflare from "@astrojs/cloudflare";
+import tailwindcss from "@tailwindcss/vite";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true,
-    },
+  output: "static",
 
-    imageService: "cloudflare",
-  }),
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": "/src",
+      },
+    },
+  },
+
+  integrations: [icon()],
 });
