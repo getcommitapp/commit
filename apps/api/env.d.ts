ca245e107871cd280b863fc13ec6b485ce7fc91d
Leonard <73912641+leonardcser@users.noreply.github.com>
2025-08-27 15:55:02 +0200

    test(api): setup api tests (#101)

---
+++ apps/api/env.d.ts
@@ -0,0 +1,16 @@
+/// <reference types="@cloudflare/vitest-pool-workers" />
+/// <reference path="./worker-configuration.d.ts" />
+
+declare interface Env {
+  DB: D1Database;
+  AUTH_GOOGLE_CLIENT_ID: string;
+  AUTH_GOOGLE_CLIENT_SECRET: string;
+  AUTH_APPLE_CLIENT_ID: string;
+  AUTH_APPLE_CLIENT_SECRET?: string;
+}
+
+declare module "cloudflare:test" {
+  interface ProvidedEnv extends Env {
+    DB: D1Database;
+  }
+}
